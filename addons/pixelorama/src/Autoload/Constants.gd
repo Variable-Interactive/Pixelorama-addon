911501c42daec718fd3dc9733851dcf21e927c32
Adrenesis <adrenesis@gmail.com>
2020-11-30 10:27:58 +0100

    Add forgotten Constants.gd

---
+++ addons/pixelorama/src/Autoload/Constants.gd
@@ -0,0 +1,13 @@
+extends Node
+
+enum Grid_Types {CARTESIAN, ISOMETRIC, ALL}
+enum Pressure_Sensitivity {NONE, ALPHA, SIZE, ALPHA_AND_SIZE}
+enum Direction {UP, DOWN, LEFT, RIGHT}
+enum Theme_Types {DARK, BLUE, CARAMEL, LIGHT}
+enum Tile_Mode {NONE, BOTH, XAXIS, YAXIS}
+enum ExportTab { FRAME = 0, SPRITESHEET = 1, ANIMATION = 2 }
+enum AnimationType { MULTIPLE_FILES = 0, ANIMATED = 1 }
+enum AnimationDirection { FORWARD = 0, BACKWARDS = 1, PING_PONG = 2 }
+enum FileFormat { PNG = 0, GIF = 1}
+
+
